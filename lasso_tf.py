e08dbf197f7e4089f0869c53a787531d9b26f83f
Hannes Jensen <hannesj1@gmail.com>
2016-03-29 15:10:54 +0200

    change optimizer

--- lasso_tf.py
+++ lasso_tf.py
@@ -3,6 +3,42 @@ import pylab as pl
 import tensorflow as tf
 
 
+def fit_lasso(data, labels):
+    '''
+    Fit a Lasso model using tensorflow
+    '''
+    # Setup placeholders and variables
+    num_datapoints = data.shape[0]
+    num_features = data.shape[1]
+    x = tf.placeholder(tf.float32, [None, num_features])
+    y_ = tf.placeholder(tf.float32, [None])
+    coeffs = tf.Variable(tf.random_normal(shape=[num_features, 1]))
+    bias = tf.Variable(tf.zeros([1]))
+
+    # Prediction
+    y = tf.matmul(x, coeffs) + bias
+
+    # Cost function
+    lasso_cost = tf.square(y-y_)
+
+    # Minimizer
+    LEARNING_RATE = 1e-11
+    NUM_STEPS = 100
+    #optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE)
+    optimizer = tf.train.AdamOptimizer()
+    train_step = optimizer.minimize(lasso_cost)
+
+    # Fit the model
+    init = tf.initialize_all_variables()
+    sess = tf.Session()
+    sess.run(init)
+    for i in range(NUM_STEPS):
+        if i % 10 == 0:
+            print 'Step:', i
+        sess.run(train_step, feed_dict={x: data, y_: labels})
+
+    return coeffs.eval(session=sess)
+
 
 # ---------- For testing ----------------
 
@@ -10,7 +46,7 @@ def fit_lasso_scikit_learn(data, labels):
     '''
     '''
     from sklearn import linear_model
-    lasso_model = linear_model.Lasso(alpha=0.001)
+    lasso_model = linear_model.Lasso(alpha=0.0001)
     lasso_model.fit(X=data, y=labels)
     return lasso_model
 
@@ -27,7 +63,7 @@ def get_test_model(n_features=100):
     return func
 
 
-def get_dataset(func, n_features=100, n_datapoints=1e5):
+def get_dataset(func, n_features=100, n_datapoints=1e4):
     '''
     Generate a test set with the given dimensions,
     using a test model.
--- tests.py
+++ tests.py
@@ -3,10 +3,15 @@ import pylab as pl
 
 if __name__ == '__main__':
     func = lasso_tf.get_test_model()
-    dataset_train, labels_train = lasso_tf.get_dataset(func, n_datapoints=1e5)
-    dataset_test, labels_test = lasso_tf.get_dataset(func, n_datapoints=1e4)
+    dataset_train, labels_train = lasso_tf.get_dataset(func, n_datapoints=1e2)
+    dataset_test, labels_test = lasso_tf.get_dataset(func, n_datapoints=1e2)
     lasso = lasso_tf.fit_lasso_scikit_learn(dataset_train, labels_train)
 
-    pl.plot(func.coeffs, 'bo', label='true')
-    pl.plot(lasso.coef_, 'ro', label='fit')
+    tf_coeffs = lasso_tf.fit_lasso(dataset_train, labels_train)
+    print tf_coeffs
+
+    pl.plot(func.coeffs, 'b-^', label='true')
+    pl.plot(lasso.coef_, 'r-x', label='scikit')
+    pl.plot(tf_coeffs, 'g-o', label='tf')
+    pl.legend(loc='best')
     pl.show()
