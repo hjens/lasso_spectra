34610e589f6054902255bede5c203c80d09e76d5
Hannes <hannesj1@gmail.com>
2016-04-14 11:58:04 +0200

    added get_design_matrix method

--- datasource.py
+++ datasource.py
@@ -1,6 +1,12 @@
 import numpy as np
+from numpy.lib.recfunctions import append_fields
 
 class DataSource:
+    pass
+
+
+class NarrowSpectraTable:
+
     def __init__(self, filename, **kwargs):
         '''
         Initialize a data source with a csv file
@@ -10,4 +16,45 @@ class DataSource:
              The file to read
         '''
         delimiter = kwargs.get('delimiter', ',')
-        data = np.genfromtxt(filename, **kwargs)
+        self.data = np.genfromtxt(filename, names=True,
+                                  delimiter=delimiter, **kwargs)
+        self.colnames = self.data.dtype.names
+
+        self._add_unique_id_col()
+
+
+    def get_design_matrix(self, target_col, feature_col):
+        '''
+        Generate a design matrix
+
+        :param target_col:
+        :param feature_col:
+        :return:
+        '''
+        design_matrix = np.zeros((self.num_objs, self.num_features))
+        for i in range(self.num_objs):
+            idx = self.data['unique_id'] == i
+            design_matrix[i,:] = self.data[feature_col][idx]
+
+        return design_matrix
+
+
+    def _add_unique_id_col(self, id_name1='gal_id', id_name2='fesc'):
+        '''
+        Add a new column to the data, with an integer
+        that uniquely identifies the object
+
+        :param id_name1:
+        :param id_name2:
+        :return:
+        '''
+        # Figure out how many unique objects there are
+        col1 = self.data[id_name1]
+        col2 = self.data[id_name2]
+        first_obj = col1[(col1==col1[0])*(col2==col2[0])]
+        self.num_features = len(first_obj)
+        self.num_objs = len(np.unique(col1))*len(np.unique(col2))
+
+        # Make unique index
+        unique_id = np.repeat(np.arange(self.num_objs), self.num_features)
+        self.data = append_fields(self.data, 'unique_id', unique_id)
--- tests/test_datasource.py
+++ tests/test_datasource.py
@@ -1,11 +1,22 @@
 import sys
 sys.path.append('..')
 import datasource
-
+import pylab as pl
 
 def test_load_file():
-    data_source = datasource.DataSource('../FiGeLMC_R100_sn5.csv')
+    #data_source = datasource.DataSource('../FiGeLMC_R100_sn5.csv')
+    #print data_source.colnames
+    #print data_source.data['gal_id']
+    pass
 
+def test_narrow_spectra():
+    spectra = datasource.NarrowSpectraTable('../FiGeLMC_R100_sn5.csv')
+    design_matrix = spectra.get_design_matrix(target_col='fesc',
+                                              feature_col='flux_noisy')
+    for i in [0, 100, 300, 600, 800, -1]:
+        pl.plot(design_matrix[i,:])
+    pl.show()
 
 if __name__ == '__main__':
     test_load_file()
+    test_narrow_spectra()
