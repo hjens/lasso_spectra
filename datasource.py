a512674b5fa31a4b1cf92eb1f0804404f13d714b
Hannes <hannesj1@gmail.com>
2016-04-14 13:42:04 +0200

    data source can split data set

--- datasource.py
+++ datasource.py
@@ -3,13 +3,32 @@ from numpy.lib.recfunctions import append_fields
 
 
 class DataSource:
-    design_matrix = None
-    target_vector = None
+    data = None
+    target = None
+    training_data = None
+    training_target = None
+    test_data = None
+    test_target = None
     num_features = 0
     num_objs = 0
 
     def __init__(self):
         pass
+
+    def shuffle_data(self):
+        indices = np.arange(self.num_objs)
+        np.random.shuffle(indices)
+        self.data = self.data[indices, :]
+        self.target = self.target[indices]
+
+    def split_data(self, training_fraction=0.8):
+        if training_fraction < 0. or training_fraction > 1.0:
+            raise ValueError('training_fraction must be between 0 and 1')
+        num_train_objs = int(self.num_objs*training_fraction)
+        self.training_data = self.data[:num_train_objs,:]
+        self.training_target = self.target[:num_train_objs]
+        self.test_data = self.data[num_train_objs:,:]
+        self.test_target = self.target[num_train_objs:]
 
 
 class NarrowSpectraTable (DataSource):
@@ -22,13 +41,12 @@ class NarrowSpectraTable (DataSource):
             filename: string
              The file to read
         '''
-        self.data = np.genfromtxt(filename, names=True,
+        self.raw_data = np.genfromtxt(filename, names=True,
                                   delimiter=',')
-        self.colnames = self.data.dtype.names
+        self.colnames = self.raw_data.dtype.names
 
         self._make_design_matrix(target_col=target_name, feature_col=feature_name,
                                  id_name1='gal_id', id_name2=target_name)
-
 
     def _make_design_matrix(self, target_col, feature_col,
                             id_name1='gal_id', id_name2='fesc'):
@@ -40,20 +58,20 @@ class NarrowSpectraTable (DataSource):
         :return:
         '''
         # Figure out how many unique objects there are
-        col1 = self.data[id_name1]
-        col2 = self.data[id_name2]
+        col1 = self.raw_data[id_name1]
+        col2 = self.raw_data[id_name2]
         first_obj = col1[(col1 == col1[0]) * (col2 == col2[0])]
         self.num_features = len(first_obj)
         self.num_objs = len(np.unique(col1)) * len(np.unique(col2))
 
         # Make unique index
         unique_id = np.repeat(np.arange(self.num_objs), self.num_features)
-        self.data = append_fields(self.data, 'unique_id', unique_id)
+        self.raw_data = append_fields(self.raw_data, 'unique_id', unique_id)
 
         # Create design matrix and target vector
-        self.design_matrix = np.zeros((self.num_objs, self.num_features))
-        self.target_vector = np.zeros(self.num_objs)
+        self.data = np.zeros((self.num_objs, self.num_features))
+        self.target = np.zeros(self.num_objs)
         for i in range(self.num_objs):
-            idx = self.data['unique_id'] == i
-            self.design_matrix[i,:] = self.data[feature_col][idx]
-            self.target_vector[i] = self.data[target_col][idx][0]
+            idx = self.raw_data['unique_id'] == i
+            self.data[i, :] = self.raw_data[feature_col][idx]
+            self.target[i] = self.raw_data[target_col][idx][0]
--- tests/test_datasource.py
+++ tests/test_datasource.py
@@ -3,18 +3,17 @@ sys.path.append('..')
 import datasource
 import pylab as pl
 
-def test_load_file():
-    #data_source = datasource.DataSource('../FiGeLMC_R100_sn5.csv')
-    #print data_source.colnames
-    #print data_source.data['gal_id']
-    pass
 
 def test_narrow_spectra():
     spectra = datasource.NarrowSpectraTable('../FiGeLMC_R100_sn5.csv')
+    spectra.shuffle_data()
+    spectra.split_data()
+    print spectra.target
     for i in [0, 100, 300, 600, 800, -1]:
-        pl.plot(spectra.design_matrix[i,:])
+        pl.plot(spectra.data[i, :], label='%.1f' % spectra.target[i])
+    pl.legend()
     pl.show()
 
+
 if __name__ == '__main__':
-    test_load_file()
     test_narrow_spectra()
