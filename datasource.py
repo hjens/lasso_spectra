095cfa29e3a7e436deb4d29cc8040c09fdb3aa8f
Hannes <hannesj1@gmail.com>
2016-04-14 13:03:53 +0200

    restructure data source

--- datasource.py
+++ datasource.py
@@ -1,13 +1,20 @@
 import numpy as np
 from numpy.lib.recfunctions import append_fields
 
+
 class DataSource:
-    pass
+    design_matrix = None
+    target_vector = None
+    num_features = 0
+    num_objs = 0
+
+    def __init__(self):
+        pass
 
 
-class NarrowSpectraTable:
+class NarrowSpectraTable (DataSource):
 
-    def __init__(self, filename, **kwargs):
+    def __init__(self, filename, target_name='fesc', feature_name='flux_noisy'):
         '''
         Initialize a data source with a csv file
 
@@ -15,15 +22,16 @@ class NarrowSpectraTable:
             filename: string
              The file to read
         '''
-        delimiter = kwargs.get('delimiter', ',')
         self.data = np.genfromtxt(filename, names=True,
-                                  delimiter=delimiter, **kwargs)
+                                  delimiter=',')
         self.colnames = self.data.dtype.names
 
-        self._add_unique_id_col()
+        self._make_design_matrix(target_col=target_name, feature_col=feature_name,
+                                 id_name1='gal_id', id_name2=target_name)
 
 
-    def get_design_matrix(self, target_col, feature_col):
+    def _make_design_matrix(self, target_col, feature_col,
+                            id_name1='gal_id', id_name2='fesc'):
         '''
         Generate a design matrix
 
@@ -31,30 +39,21 @@ class NarrowSpectraTable:
         :param feature_col:
         :return:
         '''
-        design_matrix = np.zeros((self.num_objs, self.num_features))
-        for i in range(self.num_objs):
-            idx = self.data['unique_id'] == i
-            design_matrix[i,:] = self.data[feature_col][idx]
-
-        return design_matrix
-
-
-    def _add_unique_id_col(self, id_name1='gal_id', id_name2='fesc'):
-        '''
-        Add a new column to the data, with an integer
-        that uniquely identifies the object
-
-        :param id_name1:
-        :param id_name2:
-        :return:
-        '''
         # Figure out how many unique objects there are
         col1 = self.data[id_name1]
         col2 = self.data[id_name2]
-        first_obj = col1[(col1==col1[0])*(col2==col2[0])]
+        first_obj = col1[(col1 == col1[0]) * (col2 == col2[0])]
         self.num_features = len(first_obj)
-        self.num_objs = len(np.unique(col1))*len(np.unique(col2))
+        self.num_objs = len(np.unique(col1)) * len(np.unique(col2))
 
         # Make unique index
         unique_id = np.repeat(np.arange(self.num_objs), self.num_features)
         self.data = append_fields(self.data, 'unique_id', unique_id)
+
+        # Create design matrix and target vector
+        self.design_matrix = np.zeros((self.num_objs, self.num_features))
+        self.target_vector = np.zeros(self.num_objs)
+        for i in range(self.num_objs):
+            idx = self.data['unique_id'] == i
+            self.design_matrix[i,:] = self.data[feature_col][idx]
+            self.target_vector[i] = self.data[target_col][idx][0]
--- tests/test_datasource.py
+++ tests/test_datasource.py
@@ -11,10 +11,8 @@ def test_load_file():
 
 def test_narrow_spectra():
     spectra = datasource.NarrowSpectraTable('../FiGeLMC_R100_sn5.csv')
-    design_matrix = spectra.get_design_matrix(target_col='fesc',
-                                              feature_col='flux_noisy')
     for i in [0, 100, 300, 600, 800, -1]:
-        pl.plot(design_matrix[i,:])
+        pl.plot(spectra.design_matrix[i,:])
     pl.show()
 
 if __name__ == '__main__':
